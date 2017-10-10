5a363426f02e6c83aeaed65fbc9dad3811b9fcad
manderson18 <ma128@pvsd.org>
2017-10-10 08:51:33 -0400

    Beep

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,7 +8,19 @@ public class Main {
 	Scanner kbin = new Scanner(System.in);
         System.out.println("Welcome to my fancy pantsy number generator, please enter your full name below to see your very own lucky number");
         String userName = kbin.nextLine();
-        System.out.println("Nice to meet you "+ userName+"! Your lucky number is"+perfectRandom+".\n If you would like another number, please press 2 \nIf you forgot what your name was, please press 1 \nIf you want to leave, press 0.");
+        numGen userLucky = new numGen();
+        System.out.println("Nice to meet you "+ userName+"! Your lucky number is"+userLucky.perfectRandom()+".\n If you would like another number, please press 2 \nIf you forgot what your name was, please press 1 \nIf you want to leave, press 0.");
+        String userResponse = kbin.nextLine();
+
+        if(userResponse.equalsIgnoreCase("0")){
+            System.out.println("Ok, CYA");
+
+
+        } else if (userResponse.equalsIgnoreCase("1")){
+            System.out.println("Your name is"+userName+".");
+
+
+        }
 
 
     }
--- src/com/company/numGen.java
+++ src/com/company/numGen.java
@@ -4,28 +4,17 @@ package com.company;
  * Created by ma128 on 10/10/17.
  */
 public class numGen {
+public double lucky;
+public double luckyNum = (int)(Math.random()*100);
 
+public numGen(){
 
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+public double perfectRandom(){
+        lucky = Math.pow(luckyNum, 2);
+                return luckyNum;
+    }
 
 
 
